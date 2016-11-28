cb8ce72e620bbd872321555e174a9298d858adad
navijain90 <navneet.jain@sjsu.edu>
2016-11-27 16:08:17 -0800

    Support added for n number of destinations

--- Code/BusinessLogic.py
+++ Code/BusinessLogic.py
@@ -6,20 +6,20 @@ import argparse
 from ortools.constraint_solver import pywrapcp
 from ortools.constraint_solver import routing_enums_pb2
 
-
-userDestination=raw_input("Please enter number of destination you are going\n")
-
 parser = argparse.ArgumentParser()
-parser.add_argument('--Destiantion', default = userDestination, type = int,
-                     help='No of Destination User wants to travel.')
-parser.add_argument('--tsp_use_random_matrix', default=True, type=bool,
-                     help='Use random cost matrix.')
-parser.add_argument('--tsp_random_forbidden_connections', default = 0,
-                    type = int, help='Number of random forbidden connections.')
-parser.add_argument('--tsp_random_seed', default = 0, type = int,
-                    help = 'Random seed.')
-parser.add_argument('--light_propagation', default = False,
-                    type = bool, help = 'Use light propagation')
+#userDestination=raw_input("Please enter number of destination you are going\n")
+def setParameters(userDestination):
+
+    parser.add_argument('--Destiantion', default = userDestination, type = int,
+                         help='No of Destination User wants to travel.')
+    parser.add_argument('--tsp_use_random_matrix', default=True, type=bool,
+                         help='Use random cost matrix.')
+    parser.add_argument('--tsp_random_forbidden_connections', default = 0,
+                        type = int, help='Number of random forbidden connections.')
+    parser.add_argument('--tsp_random_seed', default = 0, type = int,
+                        help = 'Random seed.')
+    parser.add_argument('--light_propagation', default = False,
+                        type = bool, help = 'Use light propagation')
 
 
 Uber =[]
@@ -38,7 +38,7 @@ def Optimalprice(DestinationList):
     Tsp_cor = []
     list = []
 
-    obj = try_tsp.RandomMatrix(4, 0, DestinationList)
+    obj = try_tsp.RandomMatrix(len(DestinationList), 0, DestinationList)
     Tsp_cor = try_tsp.tsp(parser.parse_args(), DestinationList)
     print Tsp_cor  #print list of path traversed
 
--- Code/main.py
+++ Code/main.py
@@ -14,6 +14,7 @@ import requests
 import requests
 import Lyft
 import uber
+import BusinessLogic
 
 app = Flask(__name__)
 
@@ -32,6 +33,7 @@ def price():
     locationList = [source]
     DestList = dest.split(';')
     locationList.extend(DestList)
+    BusinessLogic.setParameters(len(locationList))
 
     lyftOptimalPathList = Lyft.lyftPrice(locationList)
     uberOptimalPathList, cordinateList, priceList, serviceNameList = uber.uberPrice(locationList)
